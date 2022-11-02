d69e826a39f87e77ee16912a64245e37fb85a492
Benjamin J. Meyer <benmeyer@outlook.de>
2022-11-02 21:04:17 +0100

    First prototype

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
 }
+
+bool inProgress = false;
+final progressValue = ValueNotifier<double>(0.0);
+double eta = 0.0;
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,33 +21,20 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.blueGrey.shade800,
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.white),
+          bodyText1: TextStyle(color: Colors.white),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Stable Diffusion MobileUI'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +43,178 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  String imageDataBase64 = 'NONE';
+  var promptController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+          mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Padding(
+              padding: const EdgeInsets.fromLTRB(10, 20, 10, 10),
+              child: TextFormField(
+                controller: promptController,
+                style: const TextStyle(color: Colors.white),
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blue),
+                  ),
+                  labelStyle: TextStyle(color: Colors.white),
+                  labelText: 'Image prompt',
+                ),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Padding(
+                padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+                child: () {
+                  if (imageDataBase64 == 'NONE') {
+                    return const Padding(
+                      padding: EdgeInsets.fromLTRB(10, 200, 10, 200),
+                      child: Text(
+                        'Waiting for 1st image',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    );
+                  } else {
+                    return Image.memory(base64Decode(imageDataBase64));
+                  }
+                }()),
+            ValueListenableBuilder(
+              valueListenable: progressValue,
+              builder: (context, value, child) {
+                return Padding(
+                  padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+                  child: LinearProgressIndicator(
+                    value: value,
+                    semanticsLabel: "kkd",
+                  ),
+                );
+              },
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          createImage(promptController.text).then((value) => {
+                setState(() {
+                  var image = value;
+                  if (value.contains(",")) {
+                    image = value.split(",")[1];
+                  }
+                  imageDataBase64 = image;
+                })
+              });
+        },
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(Icons.start),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class ProgressBarIndicator extends StatefulWidget {
+  const ProgressBarIndicator({Key? key}) : super(key: key);
+
+  @override
+  State<ProgressBarIndicator> createState() => _ProgressBarIndicatorState();
+}
+
+class _ProgressBarIndicatorState extends State<ProgressBarIndicator>
+    with TickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return LinearProgressIndicator(
+      value: controller.value,
+      semanticsLabel: 'Linear progress indicator',
+    );
+  }
+}
+
+Future<String> createImage(String prompt) async {
+  var map = <String, String>{};
+
+  print("Starting request");
+
+  map['enable_hr'] = "false";
+  map['denoising_strength'] = "0";
+  map['prompt'] = 'masterpiece, best quality, masterpiece, $prompt';
+  map['seed'] = "-1";
+  map['subseed'] = "-1";
+  map['batch_size'] = "1";
+  map['steps'] = "20";
+  map['cfg_scale'] = "20";
+  map['width'] = "512";
+  map['height'] = "512";
+  map['negative_prompt'] =
+      "lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality, normal quality, jpeg artifacts, signature, watermark, username, blurry, artist name";
+  map['sampler_index'] = "Euler";
+
+  var body = json.encode(map);
+
+  var url = Uri(
+      scheme: "http",
+      host: "192.168.7.148",
+      port: 7860,
+      path: "/sdapi/v1/txt2img");
+
+  inProgress = true;
+  progressValue.value = 0;
+  doProgress();
+  http.Response response = await http
+      .post(url, body: body, headers: {"Content-Type": "application/json"});
+
+  var images = jsonDecode(utf8.decode(response.bodyBytes))['images'];
+
+  inProgress = false;
+  progressValue.value = 1;
+  return images[0];
+}
+
+Future<Map<String, double>> getProgress() async {
+  var url = Uri(
+      scheme: "http",
+      host: "192.168.7.148",
+      port: 7860,
+      path: "/sdapi/v1/progress");
+  http.Response response = await http.get(url);
+
+  var jsonResp = jsonDecode(utf8.decode(response.bodyBytes));
+  return {
+    "progress": jsonResp['progress'],
+    "eta_relative": jsonResp['eta_relative'],
+  };
+}
+
+Future<void> doProgress() async {
+  while (true) {
+    await Future.delayed(
+      const Duration(milliseconds: 500),
+      () {
+        if (!inProgress) {
+          progressValue.value = 1;
+          return;
+        }
+        getProgress().then(
+          (value) => {
+            progressValue.value = value['progress']!,
+            print("Setting ${(value['progress']! / value['eta_relative']!)} from ${value['progress']} / ${value['eta_relative']}")
+          },
+        );
+      },
     );
   }
 }
